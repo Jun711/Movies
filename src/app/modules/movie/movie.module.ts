63bddd01660cad0bf3a1c4b32d0b128204e47775
Jun <cygoh@umich.edu>
2018-02-16 21:05:41 -0800

    feat(movie module): add movie module

---
+++ src/app/modules/movie/movie.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DropDownListComponent } from '../../components/drop-down-list/drop-down-list.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    DropDownListComponent
+  ]
+})
+export class MovieModule { }
