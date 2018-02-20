50ea7de7f1ea51f201a1208c53078b0813d42233
Jun <cygoh@umich.edu>
2018-02-20 10:34:49 -0800

    test(mockData): add movies mockData for DropDownList testing

---
+++ src/app/mockData/movies.ts
@@ -0,0 +1,11 @@
+import { ListItem } from '../models/list-item.model';
+
+export const movies: ListItem[] = [
+  {id: '1', data: 'Avengers'},
+  {id: '2', data: 'Avengers2'},
+  {id: '3', data: 'Avengers3'},
+  {id: '4', data: 'Avengers4'},
+  {id: '5', data: 'Avengers5'},
+  {id: '6', data: 'Avengers6'},
+  {id: '7', data: 'Avengers7'},
+];
